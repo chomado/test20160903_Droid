8920c3dda635da23ace88233ff68515d05d4dd45
chomado <chomado@gmail.com>
2016-09-03 14:37:41 +0900

    to make each button work

--- test20160903_Droid/MainActivity.cs
+++ test20160903_Droid/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
 
 namespace test20160903_Droid
 {
@@ -16,6 +17,49 @@ namespace test20160903_Droid
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
+            // ロードされたレイアウトからUIコントロールを取得します
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            Button callButton = FindViewById<Button>(Resource.Id.CallButton);
+
+            callButton.Enabled = false;
+
+            var translatedNumber = string.Empty;
+
+            translateButton.Click += (sender, e) => 
+            {
+                // ユーザーのアルファベットの電話番号を電話番号に変換します。
+                translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+
+                if (string.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    callButton.Text = "Call";
+                    callButton.Enabled = false;
+                }
+                else
+                {
+                    callButton.Text = "Call " + translatedNumber;
+                    callButton.Enabled = true;
+                }
+            };
+
+            callButton.Click += (sender, e) =>
+            {
+                // "Call" ボタンがクリックされたら電話番号へのダイヤルを試みます。
+                var callDialog = new AlertDialog.Builder(this);
+                callDialog.SetMessage("Call " + translatedNumber + "?");
+                callDialog.SetNeutralButton("Call", delegate
+                {
+                    // 電話へのを intent 作成します
+                    var callIntent = new Intent(Intent.ActionCall);
+                    callIntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNumber));
+                    StartActivity(callIntent);
+                });
+                callDialog.SetNegativeButton("Cancel", delegate { });
+
+                // アラートダイアログを表示し、ユーザのレスポンスを待ちます
+                callDialog.Show();
+            };
         }
     }
 }
