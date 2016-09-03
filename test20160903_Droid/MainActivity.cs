f6b190ea5a4e272130718d41761f1693739516ee
chomado <chomado@gmail.com>
2016-09-03 15:26:55 +0900

    CallHistoryActivity

--- test20160903_Droid/MainActivity.cs
+++ test20160903_Droid/MainActivity.cs
@@ -2,12 +2,15 @@
 using Android.Widget;
 using Android.OS;
 using Android.Content;
+using System.Collections.Generic;
 
 namespace test20160903_Droid
 {
     [Activity(Label = "Phoneword_0903", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
+        static readonly List<string> phoneNumbers = new List<string>();
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -19,7 +22,8 @@ namespace test20160903_Droid
             EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
             Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
             Button callButton = FindViewById<Button>(Resource.Id.CallButton);
-
+            Button callHistoryButton = FindViewById<Button>(Resource.Id.CallHistoryButton);
+            
             callButton.Enabled = false;
 
             var translatedNumber = string.Empty;
@@ -48,6 +52,11 @@ namespace test20160903_Droid
                 callDialog.SetMessage("Call " + translatedNumber + "?");
                 callDialog.SetNeutralButton("Call", delegate
                 {
+                    // 掛けた番号のリストに番号を追加します。 
+                    phoneNumbers.Add(translatedNumber); 
+                    // Call History ボタンを有効にします。 
+                    callHistoryButton.Enabled = true;
+
                     // 電話へのを intent 作成します
                     var callIntent = new Intent(Intent.ActionCall);
                     callIntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNumber));
@@ -57,6 +66,14 @@ namespace test20160903_Droid
 
                 // アラートダイアログを表示し、ユーザのレスポンスを待ちます
                 callDialog.Show();
+            };
+
+
+            callHistoryButton.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(CallHistoryActivity));
+                intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
+                StartActivity(intent);
             };
         }
     }
